d210206fab1479733b570c90ff39569af1f0fe10
stefanpalmer <palmerstefan@yahoo.com>
2017-12-02 21:25:48 +0100

    Create book and story models

---
+++ src/app/anthologies/story.model.ts
@@ -0,0 +1,4 @@
+export class Story {
+  constructor (public title: string, public author: string,
+    public original: boolean) {}
+}
---
+++ src/app/shared/book.model.ts
@@ -0,0 +1,8 @@
+import { Story } from '../anthologies/story.model';
+
+export class Book {
+  constructor (public title: string, public author: string,
+    public publisher: string, public year: number, public pages: number,
+    public series: string, public seriesnum: number, public stories: Story[],
+    public isbn: number, public review: string) {}
+}
