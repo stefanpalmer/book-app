8c834fe3189b1b5066eb6ae7d94663b580996711
stefanpalmer <palmerstefan@yahoo.com>
2017-12-06 19:26:48 +0100

    Create anthology form (reactive)

--- src/app/anthologies/anthologies.component.ts
+++ src/app/anthologies/anthologies.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
+import { BookService } from '../shared/book.service';
 
 @Component({
   selector: 'app-anthologies',
@@ -6,10 +8,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./anthologies.component.css']
 })
 export class AnthologiesComponent implements OnInit {
+  anthologyForm: FormGroup;
 
-  constructor() { }
+  constructor(private bookService: BookService) { }
 
   ngOnInit() {
+    this.initForm();
+  }
+
+  onSubmit() {
+    this.bookService.addBook(this.anthologyForm.value);
+  }
+
+  onAddStory() {
+    (<FormArray>this.anthologyForm.get('stories')).push(
+      new FormGroup({
+        'storyTitle': new FormControl(null),
+        'storyAuthor': new FormControl(null),
+        'original': new FormControl(null)
+      })
+    );
+  }
+
+  private initForm() {
+    let anthologyTitle = '';
+    let anthologyEditor = '';
+    let anthologyPublisher = '';
+    let anthologyYear = null;
+    let anthologyPages = null;
+    let anthologyStories = new FormArray([]);
+    let anthologyIsbn = '';
+    let anthologyReview = '';
+
+    this.anthologyForm = new FormGroup({
+      'title': new FormControl(anthologyTitle),
+      'editor': new FormControl(anthologyEditor),
+      'publisher': new FormControl(anthologyPublisher),
+      'year': new FormControl(anthologyYear),
+      'pages': new FormControl(anthologyPages),
+      'stories': anthologyStories,
+      'isbn': new FormControl(anthologyIsbn),
+      'review': new FormControl(anthologyReview),
+    });
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -25,6 +26,8 @@ import { SubstringPipe } from './substring.pipe';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
     AppRoutingModule
   ],
   providers: [BookService],
